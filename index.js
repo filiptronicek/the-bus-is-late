b420000c10fb3ba258a0b980f0cf175979f66998
Filip Troníček <filip.tronicek@seznam.cz>
2021-03-09 10:51:22 +0100

    Write to file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import fs from 'fs';
 
 const url = "https://mapa.pid.cz/getData.php";
 
@@ -9,7 +10,13 @@ const url = "https://mapa.pid.cz/getData.php";
     if (response.ok) { // if HTTP-status is 200-299
         // get the response body (the method explained below)
         const data = await response.json();
-        console.log(data);
+        const date = new Date().toISOString();
+        fs.writeFile(`./data/${date}.json`, JSON.stringify(data), { flag: 'w+' }, (err) => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
     } else {
         console.log("HTTP-Error: " + response.status);
     }
